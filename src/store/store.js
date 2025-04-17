af091293b64fc466360968eba43a0b6e9d7c9f90
Adel-elbamb <engadel987654321@gmail.com>
2025-04-17 04:58:00 +0300

    final

--- src/store/store.js
+++ src/store/store.js
@@ -8,11 +8,3 @@ const store = configureStore({
 });
 
 export default store;
-// import { configureStore } from "@reduxjs/toolkit";
-// import TVwatchList from "./slices/TVWatchList.js";
-// const store = configureStore({
-//   reducer: {
-//     tvShowsList: TVwatchList,
-//   },
-// });
-// export default store;
